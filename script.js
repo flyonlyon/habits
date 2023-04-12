b269b968f78cc116082cc34428b2c9873e02a230
lfelpi-umich <lfelpi@umich.edu>
2023-04-12 12:41:37 -0400

    Changes to the website (Add To Chrome button working)

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function showPopup() {
     document.getElementById("popup").style.display = "block";
+    const a = document.createElement('a')
+    a.href = "habits-extension.zip"
+    a.download = "habits-extension.zip".split('/').pop()
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
 } // showPopup()
 
 function hidePopup() {
